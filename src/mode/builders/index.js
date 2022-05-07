f5053f3b05784b4d498c94eef2d7b7531d060ce7
mingrui123 <529539548@qq.com>
2022-05-07 16:07:20 +0800

    模板增加render 函数

--- src/mode/builders/index.js
+++ src/mode/builders/index.js
@@ -1,12 +1,34 @@
 import {useState} from "lib/useState.js";
 import 'src/mode/builders/index.less';
 
+function But () {
+    const [data, setData] = useState({
+        count: 0
+    }, But);
+
+    return {
+        data,
+        render() {
+            return (
+                <div onClick={() => {
+                    setData((data) => {
+                        data.count++;
+                        return data;
+                    });
+                }}>点我累加 [[count]]</div>
+            )
+        }
+    };
+}
+
 const Context = (props) => {
     const {setData: change} = props;
     const [data, setData] = useState({
         name: 123,
         a: <span style={{color: 'red'}}>我是span</span>,
-        b: <span style={{color: '#488375'}}>span 2</span>
+        b: <span style={{color: '#488375'}}>span 2</span>,
+        com: <But/>,
+        list: Array.from({length: 5}, (_, index) => index + 1)
     }, Context);
 
     change((data) => {
@@ -19,10 +41,12 @@ const Context = (props) => {
         render() {
             return (
                 <div onClick={() => setData({name: 1})}>
-                    我是context-[[name]] - - a[[(d) => (d.name > 2)? d.a: d.b]] --a [[name * 3]]
+                    我是context-[[name]] - 点我修改name - a[[(d) => (d.name > 2)? d.a: d.b]] --a [[name * 3]]
                     <div>
-                        [[() => <span>123123</span>]]
+                        a[[name]]b{`[[(o) => render('div', null, render('span', {style: {color: 'red'}}, o.name))]]`}
                     </div>
+                    [[(d) => d.com]]
+                    {`[[({list}) => render('div', null, list.map(res => (render('div', null, res))))]]`}
                 </div>
             );
         }
